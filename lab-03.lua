95f10c2115b1ef704e886da98928f27c0780617b
zopama <zopamamapapa@gmail.com>
2020-09-29 13:53:37 +0300

    dsadsad

--- lab-03.lua
+++ lab-03.lua
@@ -13,14 +13,15 @@ prov:load_from_path('style.css')
 ctx=gtk.StyleContext()
 scr=gdk.Screen.get_default()
 ctx.add_provider_for_screen(scr,prov,gtk.STYLE_PROVIDER_PRIORITY_APPLICATION)
-opt={ui.opt_a,ui.opt_b,ui.opt_c,ui.opt_d}
+opt={ui.opt_a,ui.opt_b,ui.opt_c,ui.opt_d,ui.opt_e,ui.opt_f,ui.opt_g,ui.opt_h}
 function titikaka()
 x=0
 y=0
+z=0
 if ui.chk_x.active==true then x=1 end
 if ui.chk_y.active==true then y=1 end
-
-v=x+y*2
+if ui.chk_z.active==true then z=1 end
+v=x+y*2+z*4
 opt[v+1].active=true
 ui.value.label="Value "..v
 end
@@ -28,6 +29,9 @@ function ui.chk_x:on_clicked(...)
 titikaka()
 end
 function ui.chk_y:on_clicked(...)
+titikaka()
+end
+function ui.chk_z:on_clicked(...)
 titikaka()
 end
 ui.wnd.title='lab-03_Zhukov'
